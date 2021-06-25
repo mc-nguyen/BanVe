d8b48983bb51b40fefbcdb11fc0e3ccfad857827
mc-nguyen <gaquangvy@gmail.com>
2021-06-25 00:43:23 -0700

    Chỉnh sửa thiết kế

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 function calculation() {
-    let sold = document.getElementById("sold").value;
+    let total = document.getElementById("total").value;
+    let rest = document.getElementById("rest").value;
     let money = document.getElementById("money").value;
     let changes = document.getElementById("changes").value;
 
-    document.getElementById("tickets").innerHTML = 25 * sold;
+    document.getElementById("sold").innerHTML = total - rest;
+    document.getElementById("tickets").innerHTML = 25 * (total - rest);
     document.getElementById("profit").innerHTML = money - changes;
-    document.getElementById("donation").innerHTML = money - changes - 25 * sold;
+    document.getElementById("donation").innerHTML = money - changes - 25 * (total - rest);
 }
